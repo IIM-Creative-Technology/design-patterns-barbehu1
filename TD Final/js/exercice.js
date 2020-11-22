2156b8b5ca50d9a90086466a89f0f5c768cbbb96
barbehugo28 <barbehugo28@users.noreply.github.com>
2020-11-22 21:11:40 +0100

    Update exercice.js

--- TD Final/js/exercice.js
+++ TD Final/js/exercice.js
@@ -42,7 +42,55 @@ document.getElementById("stations").addEventListener('change',function(){
             .then(data => {
                 const time = data.result.schedules
                     .map(result =>{
-                        return `<div style="display:flex;"><li style="width:50%">${result.message}</li><li style="width:50%">${result.destination}</li><br></div>`;
+                        // Etape bonus 1
+                        let minutes = parseInt(result.message.substr(0, 2))
+
+                        // Heure française
+                        const options = {
+                            timeZone:"Europe/Paris",
+                            hour12: false,
+                            hour: "2-digit",
+                            minute: "2-digit"
+                        }
+
+                        let actualTime = new Date().toLocaleTimeString("fr",options)
+
+                        if(Number.isInteger(minutes)){
+                            
+                            let actualHours = actualTime.substr(0, 2)
+                            let actualMinutes = actualTime.substr(3, 2)
+
+                            let metroMinutes = parseInt(actualMinutes) + minutes
+
+                            if(metroMinutes>=60){
+                                let metroHours = parseInt(actualHours) + 1
+                                if(metroHours>=24){
+                                    metroHours = "00"
+                                }
+                                metroMinutes-=60
+                                if(metroMinutes<10){
+                                    metroMinutes = "0" + metroMinutes
+                                }
+
+                                console.log(metroHours + ":" + metroMinutes)
+                                return `<div style="display:flex;"><li style="width:50%">${result.message + " (" + metroHours + ":" + metroMinutes + ")"}</li><li style="width:50%">${result.destination}</li><br></div>`;
+                            }
+                            else if(metroMinutes<10){
+                                metroMinutes = "0" + metroMinutes
+                                return `<div style="display:flex;"><li style="width:50%">${result.message + " (" + actualHours + ":" + metroMinutes + ")"}</li><li style="width:50%">${result.destination}</li><br></div>`;
+                            }
+                            else {
+                                console.log(actualHours + ":" + metroMinutes)
+                                return `<div style="display:flex;"><li style="width:50%">${result.message + " (" + actualHours + ":" + metroMinutes + ")"}</li><li style="width:50%">${result.destination}</li><br></div>`;
+                            }
+
+                        }
+                        else if(result.message.search("Train a l'approche") || result.message.search("Train a quai")){
+                            return `<div style="display:flex;"><li style="width:50%">${result.message + " (" + actualTime + ")"}</li><li style="width:50%">${result.destination}</li><br></div>`;
+                        }
+                        else {
+                            return `<div style="display:flex;"><li style="width:50%">${result.message}</li><li style="width:50%">${result.destination}</li><br></div>`;
+                        }
                     })
                     .join("");
                     document.getElementById("time-list").innerHTML = ('<b>Destination & prochain train :</b>' + time);
